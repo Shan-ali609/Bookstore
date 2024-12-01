947f155b28e9cb93a27349eb90f5a3399d7f1662
Shan Ali <shan.softwareengineer007@gmail.com>
2024-12-01 00:33:25 -0800

    backend connented

--- Frontend/src/components/Freebooks.jsx
+++ Frontend/src/components/Freebooks.jsx
@@ -11,7 +11,7 @@ export default function Freebooks() {
     const getbook = async()=>{
     try {
   //  const res = await axios.get('http://localhost:4001/book')
-    const res = await axios.get('https://bookstore-ecru-zeta.vercel.app/book')
+    const res = await axios.get('https://bookstore-api-mu.vercel.app/book')
 
       const data = res.data.filter((data) => data.category === "Free")
       setbook(data)
